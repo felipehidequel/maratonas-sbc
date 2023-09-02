9d9e99ac465c911a50d574d2ab6c8c0aac69d5c0
Felipe Hidequel <felipehidequel@gmail.com>
2023-09-01 23:08:53 -0300

    update

---
+++ fast_prime.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+
+    for (int i=0 ; i<n; i++){
+        long long x; cin >> x;
+
+        bool primo = true;
+
+        for(long long j = 2; j*j <= x; j++)
+            if(x % 2 == 0) primo = false;
+
+        if (primo) cout << "Prime" << endl;
+        else cout << "Not Prime" << endl;
+    }
+    return 0;
+}
